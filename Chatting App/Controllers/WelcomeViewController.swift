32fee4ee9913ba43264ba89a7c4399593772e7f4
Asad Aftab <asadaftab.tuwien@gmail.com>
2024-08-10 18:25:55 +0200

    Typing Animations, Timers and For Loops

---
+++ Chatting App/Controllers/WelcomeViewController.swift
@@ -0,0 +1,63 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat
+//
+//  Created by Asad Aftab on 10/09/2024.
+//  Copyright © 2024 Asad Aftab. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var singupButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        customizeButtonsAndView()
+    }
+    
+    
+    
+    
+    func customizeButtonsAndView(){
+        //singupButton.setTitleColor(.white, for: .normal)
+        singupButton.layer.cornerRadius = singupButton.frame.height/2
+        
+        singupButton.layer.shadowColor = UIColor.gray.cgColor
+        singupButton.layer.shadowOffset = CGSize(width: 0, height: 5)
+        singupButton.layer.shadowRadius = 10
+        singupButton.layer.shadowOpacity = 0.5
+        
+        //Add gradient
+        //let gradientLayer = CAGradientLayer()
+        //gradientLayer.colors = [UIColor.purple.cgColor, UIColor.blue.cgColor]
+        //gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+        //gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        //gradientLayer.frame = singupButton.bounds
+        //gradientLayer.cornerRadius = singupButton.layer.cornerRadius
+        //singupButton.layer.insertSublayer(gradientLayer, at: 0)
+        
+        
+        loginButton.layer.cornerRadius = singupButton.frame.height/2
+        loginButton.layer.shadowColor = UIColor.gray.cgColor
+        loginButton.layer.shadowOffset = CGSize(width: 0, height: 5)
+        loginButton.layer.shadowRadius = 10
+        loginButton.layer.shadowOpacity = 0.5
+        
+        titleLabel.textColor = .white
+        
+        // Title Animation with the use of the timer
+        let titleLabelData = titleLabel.text!
+        titleLabel.text = ""
+        var characterIndex = 0
+        for i in titleLabelData{
+            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(characterIndex), repeats: false) { (timer) in self.titleLabel.text! += String(i)
+            }
+            characterIndex += 1
+        }
+    }
+
+}
